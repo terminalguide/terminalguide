9f710132fc2e8b04affec7220b85e2fd7ca65c0c
Martin Hostettler <textshell@uchuujin.de>
2018-06-10 14:24:46 +0200

    netlify: Install dependencies for ascii-to-svg.

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
 set -e
+set -x
+
+echo $PATH
+
+go get -v github.com/asciitosvg/asciitosvg/cmd/a2s
+export PATH="$PATH:$GOPATH/bin"
+which a2s
 
 lektor plugins list
 lektor plugins reinstall
