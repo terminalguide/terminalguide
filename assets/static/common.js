091d3c441ef0f7b7015c58127eb4ddb4cbfda580
Martin Hostettler <textshell@uchuujin.de>
2019-06-01 14:55:41 +0200

    javascript: Keep currently highlighted implementation across page reloads.

--- assets/static/common.js
+++ assets/static/common.js
@@ -1,15 +1,43 @@
 "use strict";
 
+(function() {
+
+let currentHighlight = '';
+
+function highlightImpl(impl) {
+    currentHighlight = impl;
+    for (let td of document.querySelectorAll('.list-table td[data-impl]')) {
+        if (td.dataset.impl != impl) {
+            td.classList.remove('highlight');
+        }
+    }
+    if (impl) {
+        for (let td of document.querySelectorAll('.list-table td[data-impl=' + impl + ']')) {
+            td.classList.add('highlight');
+        }
+    }
+}
+
 for (let headerCell of document.querySelectorAll('.list-table th[data-impl]')) {
     headerCell.addEventListener('click', function(event) {
         let impl = event.currentTarget.dataset.impl;
-        for (let td of document.querySelectorAll('.list-table td[data-impl]')) {
-            if (td.dataset.impl != impl) {
-                td.classList.remove('highlight');
-            }
+        if (currentHighlight === impl) {
+            impl = '';
         }
-        for (let td of document.querySelectorAll('.list-table td[data-impl=' + impl + ']')) {
-            td.classList.toggle('highlight');
-        }
+        highlightImpl(impl);
+        let state = history.state || {};
+        state.impl = impl;
+        history.replaceState(state, '');
     });
 }
+
+function init() {
+    let state = history.state || {};
+    if (state.impl) {
+        highlightImpl(state.impl);
+    }
+}
+
+init();
+
+}());
